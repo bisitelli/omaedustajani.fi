c4949ad8272c55a8bca46587fd482e4dec0ebdd9
bisitelli <erik.bisi1@gmail.com>
2024-11-12 11:39:37 +0200

    updates for frontend

--- components/iftarjous-mp.js
+++ components/iftarjous-mp.js
@@ -1,48 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/millainen-mp-olet.css'; // Varmista, että CSS-tiedostosi on mukana
 
-const handleSubmit = async (formData) => {
-  try {
-    const response = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (response.ok) {
-      console.log('Sähköposti lähetetty onnistuneesti');
-    } else {
-      console.log('Virhe sähköpostin lähetyksessä');
-    }
-  } catch (error) {
-    console.error('Virhe:', error);
-  }
-};
-
 function Iftarjous({ onNextStep, name, phone, answers }) {
-  const [fade, setFade] = useState('fade-in'); // Aloitusfade-in
+  const handleSubmit = async (formData) => {
+    try {
+      const response = await fetch('/api/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
 
-  const handleButtonClick = (action) => {
+      if (response.ok) {
+        console.log('Sähköposti lähetetty onnistuneesti');
+      } else {
+        console.log('Virhe sähköpostin lähetyksessä');
+      }
+    } catch (error) {
+      console.error('Virhe:', error);
+    }
+  };
+
+  const handleButtonClick = async (action) => {
     const formData = {
       name,
       phone,
       answers,
-      action, // Lisätään action-tieto
+      action,
       surveyType: 'Moottoripyörä',
     };
     
-    handleSubmit(formData);
-    
-    setFade('fade-out'); // Aloita fade-out
-    setTimeout(() => {
-      onNextStep(); // Siirry seuraavaan vaiheeseen fade-outin jälkeen
-    }, 500); // Odota animaation loppumista
+    await handleSubmit(formData); // Send the form data
+    onNextStep(); // Move directly to the next step without fade-out
   };
 
   return (
-    <div className={`tarjous-container ${fade}`}>
+    <div className={'tarjous-container'}>
       <h2>Voita 100€ S-ryhmän lahjakortti!</h2>
       <p className='tarjous-teksti'>Kilpailuita moottoripyöräsi vakuutus If:llä. Tarjousten ottaneiden kesken arvotaan 100€ S-ryhmän lahjakortti!</p>
       <button onClick={() => handleButtonClick('pyydä tarjous')} className='tarjous-btn'>Pyydä tarjous!</button><br /><br />
--- components/question-autoilija.js
+++ components/question-autoilija.js
@@ -2,14 +2,13 @@ import React, { useState } from 'react';
 import '../styles/millainen-autoilija-olet.css';
 
 const questions = [
-  { id: 1, text: 'Minkä ikäinen olet?', options: ['18-24 vuotta', '25-45 vuotta', 'Yli 45 vuotta'] },
-  { id: 2, text: 'Millainen auto sinulla on?', options: ['Urheiluauto, nopeus ennen kaikkea!', 'Maasturi, haluan ajaa missä tahansa säässä ja maastossa.', 'Sähköauto, ympäristöystävällisyys ja uudet innovaatiot kiinnostavat.', 'Sedan/farmari, luotan klassiseen ja varmaan valintaan.'] },
-  { id: 3, text: 'Kuinka vanha autosi on?', options: ['6-10 vuotta vanha - luotettava kaveri.', '3–5 vuotta vanha – edelleen hyvässä kunnossa.', 'Yli 10 vuotta vanha – vanha, mutta uskollinen.', 'Alle 2 vuotta vanha – uutuuden huumaa!'] },
-  { id: 4, text: 'Missä ajat eniten?', options: ['Maaseudulla tai pienemmillä teillä – rauhallista, mutta joskus yllätyksiä kuten eläimiä tiellä.', 'Siellä sun täällä – ei ole tiettyä ajomaastoa.', 'Moottoriteillä tai pitkää matkaa – pitkät ajomatkat ja nopeat väylät.', 'Suunnitelen reitin vain, jos en tunne aluetta - muuten tiedän jo parhaat reitit.'] },
-  { id: 5, text: 'Miten suhtaudut aamuruuhkaan?', options: ['En stressaa – ajan rauhassa ruuhkienkin keskellä.', 'Yritän löytää oikoreitin ja välttää turhaa odottelua.', 'Pärjäilen, kunhan olen valmistautunut ja reitti on tuttu.', 'Suunnittelen reittini etukäteen ja lähden ajoissa – turvallisuus ennen kaikkea.'] },
-  { id: 6, text: 'Miten valmistaudut pitkään ajomatkaan?', options: ['Otan vain välttämättömät tavarat mukaan ja lähden matkaan.', 'Spontaani lähtö on parasta – mitä enemmän seikkailua, sen parempi.', 'Käyn läpi perusasiat, mutta en liioittele valmisteluja.', 'Tarkistan auton huolellisesti ja varmistan, että kaikki on kunnossa.'] },
-  { id: 7, text: 'Kuinka huolissasi olet onnettomuuksista?', options: ['Vakuutukset ovat ylimitoitettuja – en usko, että niitä tarvitsen.', 'Haluan olla täysin varautunut – kaikenlaiset vahingot on parasta vakuuttaa.', 'Varaudun isoihin riskeihin, mutta en stressaa liikaa pienistä asioista.', 'Ajattelen, että elämässä sattuu ja tapahtuu, enkä murehdi etukäteen.'] },
-  { id: 8, text: 'Miten tärkeänä pidät autosi vakuutusta?', options: ['Erittäin tärkeä – haluan parhaan suojan.', 'Tärkeä, mutta hinta on ratkaiseva.', 'En kovin tärkeänä, minulle riittää perustason vakuutus.'] },
+  { id: 1, text: 'Omistatko auton?', options: ['Kyllä', 'En'] },
+  { id: 2, text: 'Minkä ikäinen olet?', options: ['18-24', '25-45', 'Yli 45-vuotta']},
+  { id: 3, text: 'Millainen auto sinulla on?', options: ['Urheiluauto, nopeus ennen kaikkea!', 'Maasturi, haluan ajaa missä tahansa säässä ja maastossa.', 'Sähköauto, ympäristöystävällisyys ja uudet innovaatiot kiinnostavat.', 'Sedan/farmari, luotan klassiseen ja varmaan valintaan.'] },
+  { id: 4, text: 'Kuinka vanha autosi on?', options: ['6-10 vuotta vanha - luotettava kaveri.', '3–5 vuotta vanha – edelleen hyvässä kunnossa.', 'Yli 10 vuotta vanha – vanha, mutta uskollinen.', 'Alle 2 vuotta vanha – uutuuden huumaa!'] },
+  { id: 5, text: 'Missä ajat eniten?', options: ['Maaseudulla tai pienemmillä teillä – rauhallista, mutta joskus yllätyksiä kuten eläimiä tiellä.', 'Siellä sun täällä – ei ole tiettyä ajomaastoa.', 'Moottoriteillä tai pitkää matkaa – pitkät ajomatkat ja nopeat väylät.', 'Suunnitelen reitin vain, jos en tunne aluetta - muuten tiedän jo parhaat reitit.'] },
+  { id: 6, text: 'Miten tärkeänä pidät autosi vakuutusta?', options: ['Erittäin tärkeä – haluan parhaan suojan.', 'Tärkeä, mutta hinta on ratkaiseva.', 'En kovin tärkeänä, minulle riittää perustason vakuutus.'] },
+  { id: 7, text: 'Maksatko mielestäsi vakuutuksesta liikaa tällä hetkellä?', options: ['Kyllä', 'En'] }
 ];
 
 function Questions({ answers, setAnswers, onNextStep }) {
